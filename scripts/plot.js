079d2055cbd8d156d59fa8ff987073e82bd998c1
Micaela Mascialino <mascialinomicaela@gmail.com>
2023-07-02 20:38:36 -0300

    Cambios de gráficos

--- scripts/plot.js
+++ scripts/plot.js
@@ -28,33 +28,35 @@ d3.csv("https://mica-mascia.github.io/vd_s2_final_mascialino/data/results_global
 function createChart(key){
 	console.log(key);
 
-	let newchart = Plot.plot({
-		width: window.innerWidth-50,
-		height: window.innerHeight-50,
-		grid: true,
-		marginTop: 50,
-		marginLeft: 50,
-		marginRight: 50,
-		x: {
-			ticks: 10,
-			label: key,
-			axis: "bottom",
-		},
-		marks: [
-			Plot.image(
-				dataChart,
-				Plot.dodgeY({
-					x: key,
-					padding: 5,
-					r: 45,
-					anchor: "middle",
-					src: "imagen",
-					width: 90,
-				})
-			),
-		],
-	});
-
+	if(key == "valence_mes"){
+		let newchart = Plot.plot({
+			width: window.innerWidth-50,
+			height: window.innerHeight-50,
+			grid: true,
+			marginTop: 50,
+			marginLeft: 50,
+			marginRight: 50,
+			x: {
+				ticks: 10,
+				label: valence,
+				axis: "bottom",
+			},
+			marks: [
+				Plot.image(
+					dataChart,
+					Plot.dodgeY({
+						x: valence,
+						padding: 5,
+						r: 45,
+						anchor: "middle",
+						src: "imagen",
+						width: 90,
+					})
+				),
+			],
+		});
+	}
+	
 	d3.select("#graph svg").remove();
 	var elem = d3.select("#graph")
 		.append(() => newchart)
